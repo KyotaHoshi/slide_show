229f54dc3fff37b5991380737c95a468d0d26172
KyotaHoshi <kyota-h.8@outlook.jp>
2020-02-20 02:43:02 +0900

    より短く書いたやつ

--- main.js
+++ main.js
@@ -72,27 +72,30 @@ const imgsB = document.getElementsByClassName("img-b")
 
 let j = 0
 let k = 1
-let l = 2
-let m = imgsB.length - 1
-let n = imgsB.length - 2
-let slideLeft
-let slideRight
+let l = imgsB.length - 1
 
 
 ///////////////////////////
 // 画像のクラスの初期設定//
 //////////////////////////
 
-imgsB[0].classList.add("non-hidden")
+imgsB[j].classList.add("non-hidden")
 
 if (imgsB.length >= 2) {
-    imgsB[1].classList.add("hidden-right")
+    imgsB[k].classList.add("hidden-right")
 }
 
 if (imgsB.length >= 3) {
-    imgsB[m].classList.add("hidden-left")
+    imgsB[l].classList.add("hidden-left")
 }
 
+
+if (imgsB.length === 1) {
+    leftB.classList.add("none")
+    rightB.classList.add("none")
+} else if (imgsB.length === 2) {
+    rightB.classList.add("none")
+}
 
 //////////////////////////////////////////////////////////////
 // clickで発生する処理                          //////////////
@@ -100,16 +103,8 @@ if (imgsB.length >= 3) {
 // それによって画像の枚数を自由に設定できるようにしました  ///
 //////////////////////////////////////////////////////////////
 
-// 1枚の時
-if (imgsB.length === 1) {
-    leftB.classList.add("none")
-    rightB.classList.add("none")
-} 
-
-// 2枚の時
-else if (imgsB.length === 2) {
-    rightB.classList.add("none")
-    slideLeft = () => {
+leftB.addEventListener("click", function() {
+    if (imgsB.length === 2) {
         imgsB[j].classList.remove("non-hidden")
         imgsB[j].classList.add("hidden-left")
         imgsB[k].classList.remove("hidden-right")
@@ -118,7 +113,34 @@ else if (imgsB.length === 2) {
         rightB.classList.remove("none")
     }
 
-    slideRight = () => {
+    else if (imgsB.length >= 3) {
+        imgsB[j].classList.remove("non-hidden")
+        imgsB[j].classList.remove("low-z-index")
+        imgsB[k].classList.remove("hidden-right")
+        imgsB[k].classList.remove("low-z-index")
+        imgsB[l].classList.remove("hidden-left")
+        j++
+        k++
+        l++
+        if (j === imgsB.length) {
+            j = 0
+        }
+        if (k === imgsB.length) {
+            k = 0
+        }
+        if (l === imgsB.length) {
+            l = 0
+        } 
+        imgsB[j].classList.add("non-hidden")
+        imgsB[k].classList.add("hidden-right")
+        imgsB[k].classList.add("low-z-index")
+        imgsB[l].classList.add("hidden-left")
+    }
+    
+});
+
+rightB.addEventListener("click", function() {
+    if (imgsB.length === 2) {
         imgsB[j].classList.remove("hidden-left")
         imgsB[j].classList.add("non-hidden")
         imgsB[k].classList.remove("non-hidden")
@@ -126,158 +148,28 @@ else if (imgsB.length === 2) {
         rightB.classList.add("none")
         leftB.classList.remove("none")
     }
-} 
-
-// 3枚の時
-else if (imgsB.length === 3) {
-    slideLeft = () => {
+    
+    else if (imgsB.length >= 3 ) {
         imgsB[j].classList.remove("non-hidden")
-        imgsB[j].classList.add("hidden-left")
-        imgsB[j].classList.remove("low-z-index")
-        imgsB[k].classList.remove("hidden-right")
-        imgsB[k].classList.add("non-hidden")
-        imgsB[k].classList.remove("low-z-index")
-        imgsB[l].classList.remove("hidden-left")
-        imgsB[l].classList.add("hidden-right")
-        imgsB[l].classList.add("low-z-index")
-        if (j === 2) {
-            j = 0
-            k++
-            l++
-        } else if (k === 2) {
-            j++
-            k = 0
-            l++
-        } else if (l === 2) {
-            j++
-            k++
-            l = 0
-        }
-    }
-
-    slideRight = () => {
-        imgsB[j].classList.remove("non-hidden")
-        imgsB[j].classList.add("hidden-right")
         imgsB[j].classList.remove("low-z-index")
         imgsB[l].classList.remove("hidden-left")
-        imgsB[l].classList.add("non-hidden")
         imgsB[l].classList.remove("low-z-index")
         imgsB[k].classList.remove("hidden-right")
-        imgsB[k].classList.add("hidden-left")
-        imgsB[k].classList.add("low-z-index")
-        if (j === 0) {
-            j = 2
-            k--
-            l--
-        } else if (k === 0) {
-            j--
-            k = 2
-            l--
-        } else if (l === 0) {
-            j--
-            k--
-            l = 2
+        j--
+        k--
+        l--
+        if (j === -1) {
+            j = imgsB.length - 1
         }
-    }
-} 
-
-// 4枚の時
-else if (imgsB.length === 4) {
-    slideLeft = () => {
-        imgsB[m].classList.remove("hidden-left")
-        imgsB[j].classList.remove("non-hidden")
-        imgsB[k].classList.remove("hidden-right")
-        imgsB[l].classList.add("hidden-right")
-        if (j === imgsB.length - 1) {
-            j = 0; k++; l++; m++;
-        } else if (k === imgsB.length - 1) {
-            j++; k = 0; l++; m++;
-        } else if (l === imgsB.length - 1) {
-            j++; k++; l = 0; m++;
-        } else if (m === imgsB.length - 1) {
-            j++; k++; l++; m = 0;
+        if (k === -1) {
+            k = imgsB.length - 1
         }
+        if (l === -1) {
+            l = imgsB.length - 1
+        } 
         imgsB[j].classList.add("non-hidden")
-        imgsB[m].classList.add("hidden-left")
-    }
-
-    slideRight = () => {
-        imgsB[j].classList.remove("hidden-left")
-        imgsB[k].classList.remove("non-hidden")
-        imgsB[l].classList.remove("hidden-right")
-        imgsB[m].classList.add("hidden-left")
-        if (j === 0) {
-            j = imgsB.length - 1; k--; l--; m--;
-        } else if (k === 0) {
-            j--; k = imgsB.length - 1; l--; m--;
-        } else if (l === 0) {
-            j--; k--; l = imgsB.length - 1; m--;
-        } else if (m === 0) {
-            j--; k--; l--; m = imgsB.length - 1;
-        }
-        imgsB[k].classList.add("non-hidden")
-        imgsB[l].classList.add("hidden-right")
-    }
-} 
-
-// 5枚以上の時
-else {
-    slideLeft = () => {
-        imgsB[m].classList.remove("hidden-left")
-        imgsB[j].classList.remove("non-hidden")
-        imgsB[k].classList.remove("hidden-right")
-        imgsB[l].classList.add("hidden-right")
-        if (j === imgsB.length - 1) {
-            j = 0; k++; l++; m++; n++;
-        } else if (k === imgsB.length - 1) {
-            j++; k = 0; l++; m++; n++;
-        } else if (l === imgsB.length - 1) {
-            j++; k++; l = 0; m++; n++;
-        } else if (m === imgsB.length - 1) {
-            j++; k++; l++; m = 0; n++;
-        } else if (n === imgsB.length - 1) {
-            j++; k++; l++; m++; n = 0;
-        } else {
-            j++; k++; l++; m++; n++;
-        }
-        console.log(j);
-        console.log(k);
-        console.log(l);
-        console.log(m);
-        console.log(n);
-        console.log("--------")
-        imgsB[j].classList.add("non-hidden")
-        imgsB[m].classList.add("hidden-left")
-    }
-    
-    slideRight = () => {
-        imgsB[m].classList.remove("hidden-left")
-        imgsB[j].classList.remove("non-hidden")
-        imgsB[k].classList.remove("hidden-right")
-        imgsB[n].classList.add("hidden-left")
-        if (j === 0) {
-            j = imgsB.length - 1; k--; l--; m--; n--;
-        } else if (k === 0) {
-            j--; k = imgsB.length - 1; l--; m--; n--;
-        } else if (l === 0) {
-            j--; k--; l = imgsB.length - 1; m--; n--;
-        } else if (m === 0) {
-            j--; k--; l--; m = imgsB.length - 1; n--;
-        } else if (n === 0) {
-            j--; k--; l--; m-- ; n = imgsB.length - 1;
-        } else {
-            j--; k--; l--; m--; n--;
-        }
-        console.log(j);
-        console.log(k);
-        console.log(l);
-        console.log(m);
-        console.log(n);
-        console.log("--------")
-        imgsB[j].classList.add("non-hidden")
+        imgsB[l].classList.add("hidden-left")
+        imgsB[l].classList.add("low-z-index")
         imgsB[k].classList.add("hidden-right")
     }
-}
-
-leftB.addEventListener("click", slideLeft);
-rightB.addEventListener("click", slideRight);
+});
